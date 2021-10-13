682bb15f89b8184c95fcf1ef077f8f7f42f3f5e1
dodoflix <dogukanmetan@gmail.com>
2021-10-14 02:03:50 +0300

    Update plugin.yml

--- src/main/java/me/dodo/customjoinmessages/CustomJoinMessages.java
+++ src/main/java/me/dodo/customjoinmessages/CustomJoinMessages.java
@@ -11,7 +11,9 @@ public final class CustomJoinMessages extends JavaPlugin {
     public static boolean PAPI = false;
 
     public static boolean joinDisable = false;
+    public static boolean joinMessagesDisable = false;
     public static boolean quitDisable = false;
+    public static boolean firstJoinDisable = false;
     public static List<String> firstJoin = new ArrayList<>();
     public static List<String> joinMessages = new ArrayList<>();
     public static List<String> quitMessages = new ArrayList<>();
@@ -20,15 +22,19 @@ public final class CustomJoinMessages extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         joinDisable = getConfig().getBoolean("joinmessages.disable");
-        quitDisable = getConfig().getBoolean("joinmessages.disable");
+        joinMessagesDisable = getConfig().getBoolean("joinmessages.messages.disable");
+        firstJoinDisable = getConfig().getBoolean("joinmessages.firstjoin.disable");
+        quitDisable = getConfig().getBoolean("quitmessages.disable");
         if (!joinDisable) {
-            firstJoin = getConfig().getStringList("joinmessages.firstjoin");
-            joinMessages = getConfig().getStringList("joinmessages.messages");
+            if (!firstJoinDisable)
+                firstJoin = getConfig().getStringList("joinmessages.firstjoin.context");
+            if (!joinMessagesDisable)
+                joinMessages = getConfig().getStringList("joinmessages.messages.context");
         }
-        if(!quitDisable) {
+        if (!quitDisable) {
             quitMessages = getConfig().getStringList("quitmessages.messages");
         }
-        if(!joinDisable || !quitDisable)
+        if (!joinDisable || !quitDisable)
             if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null)
                 PAPI = true;
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
--- src/main/java/me/dodo/customjoinmessages/events/PlayerJoin.java
+++ src/main/java/me/dodo/customjoinmessages/events/PlayerJoin.java
@@ -10,14 +10,41 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if(CustomJoinMessages.joinDisable){
+        if (CustomJoinMessages.joinDisable) {
             event.setJoinMessage(null);
             return;
         }
 
-        if (CustomJoinMessages.PAPI)
-            event.setJoinMessage(PlaceholderAPI.setPlaceholders(event.getPlayer(), ChatColor.translateAlternateColorCodes('&', String.join("\n", CustomJoinMessages.joinMessages))));
-        else
-            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', String.join("\n", CustomJoinMessages.joinMessages)));
+        if (!CustomJoinMessages.firstJoinDisable)
+            if (event.getPlayer().hasPlayedBefore()) {
+                if (CustomJoinMessages.PAPI)
+                    event.setJoinMessage(
+                            PlaceholderAPI.setPlaceholders(event.getPlayer(),
+                                    ChatColor.translateAlternateColorCodes('&',
+                                            String.join("\n", CustomJoinMessages.firstJoin)))
+                    );
+                else
+                    event.setJoinMessage(
+                            ChatColor.translateAlternateColorCodes('&',
+                                    String.join("\n", CustomJoinMessages.firstJoin))
+                    );
+
+                return;
+            }
+
+
+        if (!CustomJoinMessages.joinMessagesDisable)
+            if (CustomJoinMessages.PAPI)
+                event.setJoinMessage(
+                        PlaceholderAPI.setPlaceholders(
+                                event.getPlayer(),
+                                ChatColor.translateAlternateColorCodes('&',
+                                        String.join("\n", CustomJoinMessages.joinMessages)))
+                );
+            else
+                event.setJoinMessage(
+                        ChatColor.translateAlternateColorCodes('&',
+                                String.join("\n", CustomJoinMessages.joinMessages))
+                );
     }
 }
